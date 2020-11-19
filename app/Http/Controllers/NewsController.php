d2fc1581bc8f1ed2283a659195d82f81f51e46ce
Muhammad Nur Amin <muhnuramin956@gmail.com>
2020-11-19 22:16:20 +0700

    first commit

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+use guzzleHttp\Exception\RequestException;
+
+
+class NewsController extends Controller
+{
+    public function getData(){
+        $client=new Client();
+        $request=$client->get('https://newsapi.org/v2/top-headlines?country=id&apiKey=ca4fad88b0d648419b318b6dedea128f');
+        $response=$request->getBody();
+        $result=json_decode($response);
+        return view('welcome',['artikel'=>$result->articles]);
+    }
+}
